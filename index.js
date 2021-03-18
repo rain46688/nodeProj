d9133d8c27c86bab84db656d9eaf25e8d6283e5b
rain46688 <rain46688@gmail.com>
2021-03-18 19:29:50 +0900

    로그인 부분

--- index.js
+++ index.js
@@ -26,22 +26,64 @@ app.get('/', (req, res) => {
 
 app.post('/register',(req, res) =>{
 
-  //회원 가입 필요한 정보들을 client에서 가져오면 그것을 DB에 넣어준다.
+    //회원 가입 필요한 정보들을 client에서 가져오면 그것을 DB에 넣어준다.
 
-  const user = new User(req.body)
-//req.body는 json 형식으로 넘어온다.
-//이건 bodyParser를 이용해서 클라이언트의 정보를 req로 받아주는것이라고한다.
+    const user = new User(req.body)
+  //req.body는 json 형식으로 넘어온다.
+  //이건 bodyParser를 이용해서 클라이언트의 정보를 req로 받아주는것이라고한다.
 
-//몽고 디비에있는 함수
-user.save((err, userInfo) => {
-  if(err) return res.json({success:false, err})
-  //성공 못하면 false랑 err 내용이 뜨는것
-  return res.status(200).json({//200은 성공을 의미
-    success:true
-    //성공하면 success가 true가 뜨고
+  //몽고 디비에있는 함수
+  user.save((err, userInfo) => {
+    if(err) return res.json({success:false, err})
+    //성공 못하면 false랑 err 내용이 뜨는것
+    return res.status(200).json({//200은 성공을 의미
+      success:true
+      //성공하면 success가 true가 뜨고
+    })
   })
 })
-m 
+
+app.post('/login', (req, res) => {
+  
+  //이메일 디비에서 확인
+  //몽고디비에서 제공하는 findOne 함수를 이용해서 찾기
+  //req.body.email는 넘어오는 리퀘스트에 email 값
+  User.findOne({email:req.body.email}, (err, userInfo) =>{
+    //userInfo가 없으면 로그인 실패라고 표시해줌
+    console.log("userInfo : "+userInfo)
+    //userInfo 값에는 몽고디비에서 해당 이메일에 맞는 값들을 가져와줌 role,id,name,email 등등
+    if(!userInfo){
+      return res.json({
+        loginSuccess:false,
+        message:"로그인 실패"
+      })
+    }
+ //비밀번호 확인
+ //comparePassword 함수는 Users.js에서 만듬
+    userInfo.comparePassword(req.body.password,(err, isMatch) =>{
+      console.log("isMatch : "+isMatch)
+
+      //isMatch가 false인경우
+      if(!isMatch)
+      return res.json({
+        loginSuccess:false, 
+        message:"비밀번호 틀림"})
+        
+        //isMatch가 true인 경우
+        return res.status(200).json({//200은 성공을 의미
+          loginSuccess:true,
+          message:"로그인 성공"
+          //성공하면 success가 true가 뜨고
+        })
+
+      //로그인 성공하면 토큰 생성하기
+      // userInfo.generateToken((err, userInfo) =>{
+        
+      // })
+
+    })
+
+  })
 
 })
 
--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+// 10글자의 솔트값을 만든다는 의미
+const saltRounds = 10
 
 const userSchma = mongoose.Schema({
     name:{
@@ -24,6 +27,45 @@ const userSchma = mongoose.Schema({
         type: Number
     }
 })
+
+//save하기 전에 즉 몽고 디비에 넣기 전에 처리할 함수를 적는것
+//비밀번호를 암호화하려면 디비에 넣기 전에 해야되기때문이다.
+userSchma.pre('save',function( next ){
+    //비밀번호를 암호화 시킨다.
+
+    let user = this;//이건 위에 모델들을 가르키는것이다. user.password하면 사용자가 입력한 비밀번호가 넘어옴
+
+    if(user.isModified('password')){
+        //패스워드가 변경될때만 실행되게 만듬
+        bcrypt.genSalt(saltRounds,function(err, salt){
+            //에러 났을 경우
+            if(err) return next(err)
+            //user.password 플래인 패스워드
+            //성공했을경우
+            bcrypt.hash(user.password, salt, function(err, hash){
+                //에러 났을경우
+                if(err) return next(err)
+                //성공했을 경우
+                user.password = hash
+                next()
+            })
+        })
+    }else {
+        next()
+    }
+})
+
+
+userSchma.methods.comparePassword = function(plainPassword,callbackFunction){
+    //플레인 패스워드를 암호화해서 디비에 비교해서 같은지 확인
+    bcrypt.compare(plainPassword,this.password, function(err,isMatch){
+        if(err) return callbackFunction(err)
+        callbackFunction(null, isMatch)
+    })
+
+}
+
+
 
 const User = mongoose.model('User',userSchma)
 module.exports = { User }
