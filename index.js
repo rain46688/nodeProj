885c49d13b34fa15fa1bc3e60831689841a01eb5
rain46688 <rain46688@gmail.com>
2021-03-22 18:25:31 +0900

    로그인 토큰 생성하기

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser')
-const {User} = require('./models/Users')
-
 const config = require('./config/key')
+const {User} = require('./models/Users')
+//cookieparser사용하기 토큰 쿠키에 저장
+const cookieParser = require('cookie-parser')
+
 
 //application/x-www-form-urlencoded 이런걸 분석해서 가져오기 위해 아래 옵션을 추가
 app.use(bodyParser.urlencoded({extended:true}))
 
 //application/json 이런걸 분석해서 가져오기 위해 아래 옵션을 추가
 app.use(bodyParser.json())
+app.use(cookieParser())
 
 const monoose = require('mongoose')
 const { json } = require('body-parser')
@@ -44,47 +47,48 @@ app.post('/register',(req, res) =>{
 })
 
 app.post('/login', (req, res) => {
-  
-  //이메일 디비에서 확인
-  //몽고디비에서 제공하는 findOne 함수를 이용해서 찾기
-  //req.body.email는 넘어오는 리퀘스트에 email 값
-  User.findOne({email:req.body.email}, (err, userInfo) =>{
-    //userInfo가 없으면 로그인 실패라고 표시해줌
-    console.log("userInfo : "+userInfo)
-    //userInfo 값에는 몽고디비에서 해당 이메일에 맞는 값들을 가져와줌 role,id,name,email 등등
-    if(!userInfo){
-      return res.json({
-        loginSuccess:false,
-        message:"로그인 실패"
-      })
-    }
- //비밀번호 확인
- //comparePassword 함수는 Users.js에서 만듬
+
+      //이메일 디비에서 확인
+      //몽고디비에서 제공하는 findOne 함수를 이용해서 찾기
+      //req.body.email는 넘어오는 리퀘스트에 email 값
+      User.findOne({email:req.body.email}, (err, userInfo) =>{
+        //userInfo가 없으면 로그인 실패라고 표시해줌
+        console.log("userInfo : "+userInfo)
+        //userInfo 값에는 몽고디비에서 해당 이메일에 맞는 값들을 가져와줌 role,id,name,email 등등
+        if(!userInfo){
+          return res.json({
+            loginSuccess:false,
+            message:"로그인 실패"
+          })
+        }
+
+        //비밀번호 확인
+    //comparePassword 함수는 Users.js에서 만듬
     userInfo.comparePassword(req.body.password,(err, isMatch) =>{
-      console.log("isMatch : "+isMatch)
+        console.log("isMatch : "+isMatch)
+        console.log("err : "+err) 
+        console.log("req.body.password : "+req.body.password)
 
-      //isMatch가 false인경우
-      if(!isMatch)
-      return res.json({
-        loginSuccess:false, 
-        message:"비밀번호 틀림"})
-        
+        //isMatch가 false인경우
+        if(!isMatch)
+        return res.json({
+          loginSuccess:false, 
+          message:"비밀번호 틀림"})
         //isMatch가 true인 경우
-        return res.status(200).json({//200은 성공을 의미
-          loginSuccess:true,
-          message:"로그인 성공"
-          //성공하면 success가 true가 뜨고
-        })
+        //로그인 성공하면 토큰 생성하기
+        userInfo.generateToken((err, userInfo) =>{
+          //err가 있으면 400 페이지랑 에러를 표시한다
+          console.log("err : "+err)
+          if(err) return res.status(400).send(err)
 
-      //로그인 성공하면 토큰 생성하기
-      // userInfo.generateToken((err, userInfo) =>{
-        
-      // })
-
+          //토큰을 쿠키나 로컬스토리지에 저장한다
+          res.cookie("loginMember",userInfo.token).status(200).json({
+            loginSuccess : true,
+            message:"로그인 성공"
+          })
+      })
     })
-
   })
-
 })
 
 app.listen(port, () => {
--- models/Users.js
+++ models/Users.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 // 10글자의 솔트값을 만든다는 의미
 const saltRounds = 10
+//jsonwebtoken 가져오기
+const jwt = require('jsonwebtoken');
 
 const userSchma = mongoose.Schema({
     name:{
@@ -13,7 +15,7 @@ const userSchma = mongoose.Schema({
         unique:1
     },password:{
         type: String,
-        maxlength: 5
+        maxlength: 500
     },lastname:{
         type: String,
         maxlength: 50
@@ -32,7 +34,7 @@ const userSchma = mongoose.Schema({
 //비밀번호를 암호화하려면 디비에 넣기 전에 해야되기때문이다.
 userSchma.pre('save',function( next ){
     //비밀번호를 암호화 시킨다.
-
+    console.log("Users.js pre실행")
     let user = this;//이건 위에 모델들을 가르키는것이다. user.password하면 사용자가 입력한 비밀번호가 넘어옴
 
     if(user.isModified('password')){
@@ -55,16 +57,37 @@ userSchma.pre('save',function( next ){
     }
 })
 
-
+//comparePassword 함수 생성
 userSchma.methods.comparePassword = function(plainPassword,callbackFunction){
+    console.log("Users.js comparePassword실행")
     //플레인 패스워드를 암호화해서 디비에 비교해서 같은지 확인
-    bcrypt.compare(plainPassword,this.password, function(err,isMatch){
+    console.log("plainPassword : "+plainPassword)
+    console.log("this.password : "+this.password)
+    bcrypt.compare(plainPassword, this.password, function(err, isMatchPassword){
+        console.log("isMatchPassword : "+isMatchPassword+", err : "+err)
+        //bcrypt의 compare함수로 플레인 패스워드를 암호화하고 기존 패스워드랑 비교
         if(err) return callbackFunction(err)
-        callbackFunction(null, isMatch)
+        //callbackFunction 콜백 함수로 err를 보내거나 null, isMatchPassword(참거짓)를 일치하냐 안하냐 보냄
+        callbackFunction(null, isMatchPassword)
+    })
+}
+
+userSchma.methods.generateToken = function(callbackFunction){
+    //jsonwebToken을 이용해서 token을 생성하기
+    //위의 모델에서 this로 가져오는것 
+    const user = this
+    //_id는 몽고 디비에 자동으로 들어가는 id 컬럼, 유저마다 식별자, 기본키 라보면됨
+   let token =  jwt.sign(user._id.toHexString(), 'cmsweb')  
+    //user._id, 'cmsweb' 둘을 합쳐서 token을 만드는것이고 'cmsweb'으로 user id를 가져올수있음
+    user.token = token
+    //몽고 디비에서 쓰는 save함수를 이용해서 디비에 token값 넣기
+    user.save(function(err, user){
+        if(err) return callbackFunction(err)
+        //에러가 없는 경우에는 에러는 null이고 user정보를 넣어줌
+        callbackFunction(null, user)
     })
 
 }
-
 
 
 const User = mongoose.model('User',userSchma)
