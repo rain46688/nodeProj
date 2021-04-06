99b1d41f0f3be4ae74c3f097c9bf4e064fa8305c
rain46688 <rain46688@gmail.com>
2021-04-06 20:03:19 +0900

    프록시 만들고 서버 클라 한번에 켜기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 function LandingPage() {
 
     useEffect(() => {
-        axios.get('http://localhost:5000/api/hello')
+        axios.get('/api/hello')
         .then(response => console.log(response.data))
     }, [])
 
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,11 @@
-const proxy = require('http-proxy-middleware');
-module.exports = function (app) {
-    app.use(
-      '/api',
-      proxy({
-          target : 'http://localhost:5000',
-          changeOrigin : true,
-      })  
-    );
-}
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:5000',
+      changeOrigin: true,
+    })
+  );
+};
